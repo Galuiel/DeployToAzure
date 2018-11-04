c57a276e7c547d1c08fcd21c9526e6e7c3a85a93
Galuiel <42965606+Galuiel@users.noreply.github.com>
2018-11-04 18:11:00 +0200

    Task-5

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+require('./app_server/models/db');
 
 const indexRouter = require('./app_server/routes/index');
 
